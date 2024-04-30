52238da7cde7526547da587418f9f286b0f0f31a
ekaterina <89221049@student.upr.si>
2024-04-30 02:41:48 +0200

    "Worker proto"

--- Genetic/src/Worker.java
+++ Genetic/src/Worker.java
@@ -1,11 +1,12 @@
 import mpi.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class Worker {
-    private ArrayList<Graph> subPopulation;
-    private GeneticAlgorithm geneticAlgorithm;
+    private final GeneticAlgorithm geneticAlgorithm;
 
     public Worker(GeneticAlgorithm geneticAlgorithm) {
         this.geneticAlgorithm = geneticAlgorithm;
@@ -13,13 +14,27 @@ public class Worker {
     }
 
     public void receivePopulationChunk() throws MPIException, IOException, ClassNotFoundException {
+        // Receive the serialized subpopulation from the master
+        byte[] serializedSubPopulation = new byte[1000];
+        MPI.COMM_WORLD.Recv(serializedSubPopulation, 0, 1000, MPI.BYTE, 0, 0);
 
+        // Deserialize the subpopulation
+        geneticAlgorithm.population = deserializeSubPopulation(serializedSubPopulation);
+
+
+        // Perform selection, crossover, and mutation
+        geneticAlgorithm.calculateFitness();
+        geneticAlgorithm.selection();
+        geneticAlgorithm.crossover();
+        geneticAlgorithm.mutation(GeneticAlgorithm.MUTATION_PROBABILITY);
     }
 
-    public void calculateFitnessForSubset() {
-        for (Graph graph : this.subPopulation) {
-            graph.fitnessEvaluation();
+    public ArrayList<Graph> deserializeSubPopulation(byte[] serializedSubPopulation) throws IOException, ClassNotFoundException {
+        ArrayList<Graph> subPopulation;
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(serializedSubPopulation); ObjectInputStream ois = new ObjectInputStream(bis)) {
+            subPopulation = (ArrayList<Graph>) ois.readObject();
         }
+        return subPopulation;
     }
 
 
