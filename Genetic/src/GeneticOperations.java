50146d941f5652b23da656892e27e1b89be68584
ekaterina <89221049@student.upr.si>
2024-05-02 22:48:08 +0200

    "interface"

---
+++ Genetic/src/GeneticOperations.java
@@ -0,0 +1,12 @@
+public interface GeneticOperations {
+
+    void crossoverOnePoint();
+
+    void mutation(double mutationRate);
+
+    void selection();
+
+    void calculateFitness();
+
+    void mutationSwap();
+}
